a5904fce3f3061fdb434a1efaf1dc089ccdbb39c
Evan Teran <evan.teran@gmail.com>
2026-07-29 22:22:53 -0400

    combining common code from Intel/Arm thread classes into a consolidated base class so it doesn't need to be repeated

--- lib/include/Debug/ThreadIntel.hpp
+++ lib/include/Debug/ThreadIntel.hpp
@@ -3,30 +3,17 @@
 #define THREAD_INTEL_HPP_
 
 #include "Context.hpp"
+#include "ThreadBase.hpp"
 
 #include <cstdint>
-#include <optional>
-#include <sys/ptrace.h>
-#include <sys/wait.h>
 
 class Process;
 
-class Thread {
+class Thread : public ThreadBase {
 	friend class Debugger;
 	friend class Process;
 
-	enum class State {
-		Stopped,
-		Running,
-	};
-
 	struct internal_t {};
-
-public:
-	using Flag                             = uint32_t;
-	static constexpr Flag Attach           = 1u << 0;
-	static constexpr Flag NoAttach         = 1u << 1;
-	static constexpr Flag KillOnTracerExit = 1u << 2;
 
 public:
 	// The constructor is private to force users to create Thread objects through the Process interface.
@@ -36,44 +23,11 @@ public:
 	~Thread();
 
 public:
-	[[nodiscard]] pid_t tid() const { return tid_; }
-	[[nodiscard]] int wait_status() const { return wstatus_; }
-
-public:
-	[[nodiscard]] bool is_exited() const;
-	[[nodiscard]] bool is_signaled() const;
-	[[nodiscard]] bool is_stopped() const;
-	[[nodiscard]] bool is_continued() const;
-
-public:
-	[[nodiscard]] int exit_status() const;
-	[[nodiscard]] int signal_status() const;
-	[[nodiscard]] int stop_status() const;
-
-public:
 	void get_context(Context *ctx) const;
 	void set_context(const Context *ctx) const;
 
-	uint64_t get_instruction_pointer() const;
-	void set_instruction_pointer(uint64_t ip) const;
-
-public:
-	void kill() const;
-	void step();
-	void resume();
-	void resume_until_syscall();
-	void step(int signal);
-	void resume(int signal);
-	void resume_until_syscall(int signal);
-	void stop() const;
-	void detach();
-	void wait();
-
-private:
-	void resume_internal(int signal, bool until_syscall);
-
-public:
-	bool load_signal_info();
+	uint64_t get_instruction_pointer() const override;
+	void set_instruction_pointer(uint64_t ip) const override;
 
 private:
 	[[nodiscard]] bool detect_64_bit() const;
@@ -104,16 +58,6 @@ private:
 	int set_xstate32_modern(const Context *ctx) const;
 	int set_xstate32_legacy(const Context *ctx) const;
 	int set_xstate32_fallback(const Context *ctx) const;
-
-private:
-	Process *process_                                = nullptr;
-	pid_t tid_                                       = 0;
-	int wstatus_                                     = 0;
-	int pending_signal_                              = 0;
-	std::optional<uint64_t> pending_step_breakpoint_ = {};
-	State state_                                     = State::Running;
-	bool is_64_bit_                                  = false;
-	siginfo_t siginfo_                               = {};
 };
 
 #endif
